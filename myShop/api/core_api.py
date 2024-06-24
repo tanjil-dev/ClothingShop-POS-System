eef4115bf452eebabe1dfad26212bd860c5d9753
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-06-24 11:33:32 +0600

    clean up

--- myShop/api/core_api.py
+++ myShop/api/core_api.py
@@ -1,7 +1,6 @@
 import json
 import re
 
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
